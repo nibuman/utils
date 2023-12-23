5770de19c48d21c013fbbd420f8317b353572a5b
Nick Bushby <nick.bushby5@gmail.com>
2023-12-23 12:58:21 +0000

    initial

---
+++ src/utils/points.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from typing import Iterable
+
+
+@dataclass
+class Vector:
+    row: int
+    col: int
+
+    def __add__(self, other):
+        return Point(self.row + other.row, self.col + other.col)
+
+    def __sub__(self, other):
+        return Point(self.row - other.row, self.col - other.col)
+
+    def __iter__(self):
+        yield self.row
+        yield self.col
+
+    def __hash__(self):
+        return hash((self.row, self.col))
+
+    def __eq__(self, other):
+        return (self.row == other.row) and (self.col) == (other.col)
+
+
+def manhattan(
+    vector1: Iterable[int | float], vector2: Iterable[int | float]
+) -> int | float:
+    r1, c1 = vector1
+    r2, c2 = vector2
+    return abs(r1 - r2) + abs(c1 - c2)
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present Nick Bushby <nick.bushby5@gmail.com>
+#
+# SPDX-License-Identifier: MIT
